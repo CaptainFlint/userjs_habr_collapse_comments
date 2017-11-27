37e610c7af83b9676114326e1ef425f93d576ea3
Konstantin Vlasov <support@flint-inc.ru>
2017-11-27 22:31:25 +0300

    Uploaded the latest available version

---
+++ habr_collapse.user.js
@@ -0,0 +1,64 @@
+﻿// ==UserScript==
+// @name            Comment collapsing
+// @description     Allows to collapse/expand comment subtrees
+// @author          Aleksey Borodin (@minamoto), Konstantin Vlasov (@CaptainFlint)
+// @namespace       Habrahabr
+// @version         1.1
+// @include         https://geektimes.ru/*
+// @include         https://habrahabr.ru/*
+// @updateURL       https://raw.githubusercontent.com/CaptainFlint/userjs_habr_collapse_comments/master/habr_collapse.user.js
+// ==/UserScript==
+
+
+"use strict";
+
+!function(win) {
+
+if (window != window.top)
+    return
+
+win.addEventListener("load", function() {
+
+    $('.voting-wjt_comments').filter(function() {
+        return $(this).parent().parent().next().children().length > 0;
+    }).before('<a href="#" class="comment_toggle" title="Свернуть ветку">[–]</a>');
+
+    $('.comment__message_banned').filter(function() {
+        return $(this).parent().next().children().length > 0;
+    }).append('<a href="#" class="comment_toggle" style="margin-left: 10px;" title="Свернуть ветку">[–]</a>');
+
+    $('.comments-section__subscribe-panel').before('<a href="#" class="all_comments_hide" style="margin-left: 10px;" title="Свернуть все ветки">[ – – – ]</a><a href="#" class="all_comments_show" style="color: red; font-weight: bold; margin-left: 10px;" title="Развернуть все ветки">[ + + + ]</a>');
+
+    $('.comment_toggle').on("click", function() {
+        $(this).parent().parent().next().toggle();
+        if ($(this).text() == '[–]') {
+            $(this).text('[+]');
+            $(this).css({'color': 'red', 'font-weight': 'bold'});
+            $(this).attr('title', "Развернуть ветку");
+        }
+        else {
+            $(this).text('[–]')
+            $(this).css({'color': '', 'font-weight': ''});
+            $(this).attr('title', "Свернуть ветку");
+        }
+    });
+
+    $('.all_comments_hide').on("click", function() {
+        $('.comments-section > ul > li > div.comment a.comment_toggle').each(function () {
+            if ($(this).text() == '[–]') {
+                $(this).trigger( "click" );
+            }
+        })
+    });
+
+    $('.all_comments_show').on("click", function() {
+        $('.comments-section > ul > li > div.comment a.comment_toggle').each(function () {
+            if ($(this).text() == '[+]') {
+                $(this).trigger( "click" );
+            }
+        })
+    });
+
+}, false);
+
+}(typeof unsafeWindow == 'undefined' ? window : unsafeWindow);
